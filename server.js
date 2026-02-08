aa550736bac095b7d637b9a24e1ca5a36e195f0e
Garvit Rai <raigarvit9@gmail.com>
2026-02-08 10:50:32 +0530

    Fixed cors

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// Middleware
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // MongoDB Connection
